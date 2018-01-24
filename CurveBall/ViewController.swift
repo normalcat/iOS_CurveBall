666a1ee55bac991678b122c739ba3ee4790be3f8
Chaoi Chen <chaoichen@icloud.com>
2018-01-24 14:17:33 -0800

    Initial Commit

---
+++ CurveBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurveBall
+//
+//  Created by Chao-I Chen on 1/24/18.
+//  Copyright © 2018 Chao-I Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
